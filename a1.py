a75ae72a56d4a2fb30abeae0594549d50f6a36a5
Kristoffer Johansson <kristoffer.johansson@gmail.com>
2020-05-17 11:56:20 +0200

    h

--- a1.py
+++ a1.py
@@ -78,17 +78,23 @@ def to_24_hour_clock(hours):
 
 
 ### Write your get_hours function definition here:
+def get_hours(seconds):
 
+    return seconds//3600
 
 
 
 ### Write your get_minutes function definition here:
+def get_minutes(seconds):
 
+    return (seconds%3600)//60
 
 
 
 ### Write your get_seconds function definition here:
+def get_seconds(seconds):
 
+    return 
 
 
 
